0cfd7e6a4415787a553509b95f6306c8d2e7b873
Mar.io <mario@stake.co.jp>
2021-11-23 14:32:22 +0100

    interface definition

--- precompiles/dapps-staking/DappsStaking.sol
+++ precompiles/dapps-staking/DappsStaking.sol
@@ -5,6 +5,9 @@ pragma solidity >=0.7.0;
 /// Interface to the precompiled contract on Shibuya/Shiden/Astar
 /// Predeployed on the address 0x0000000000000000000000000000000000005001
 interface DappsStaking {
+
+    // Storage getters
+
     /// @dev Get current era.
     /// Selector: d7be3896
     /// @return The current era
@@ -15,10 +18,28 @@ interface DappsStaking {
     /// @return The Reward and Staked amount for given era
     function era_reward_and_stake(uint32 era) external view returns (uint128, uint128);
 
-    /// @dev Get Contract address for the provided H160 accountId.
+    /// @dev Get Bonded amount for the staker (H160).
+    /// Selector: Not implemented
+    /// @return Bonded amount for the staker
+    function ledger(address staker) external view returns (uint128);
+
+    /// @dev Get Developer's address for the provided H160 contract.
+    /// Selector: Not implemented
+    /// @return address as uint256, not as H160
+    function registered_developer(address contract_id) external view returns (uint256);
+
+    /// @dev Get amount staked and stakers for a contract per era
+    /// Selector: Not implemented
+    /// The array of 2 stakers is encoded like: [staker1, amount1, staker2, amount2]
+    /// @return amount staked on contract, amount of claimed rewards, array of stakers
+    function contract_era_stake(address contract_id, uint32 era) external view returns (uint128, uint128, uint256[] memory);
+
+    /// @dev Get Contract's address for the provided H160 accountId.
     /// Selector: 192fb256
     /// @return address as uint256, not as H160
     function registered_contract(address developer) external view returns (uint256);
+
+    // Extrinsic calls
 
     /// @dev Register provided contract.
     /// Selector: 4420e486
@@ -27,4 +48,12 @@ interface DappsStaking {
     /// @dev Stake provided amount on the contract.
     /// Selector: 52b73e41
     function bond_and_stake(address, uint128) external;
+
+    /// @dev Unbond, unstake and withdraw provided amount on the contract.
+    /// Selector: Not implemented
+    function unbond_unstake_and_withdraw(address, uint128) external;
+
+    /// @dev Claim contract's rewards.
+    /// Selector: Not implemented
+    function claim(address) external;
 }
